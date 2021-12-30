cceca1462acd068ddd8dafe34030446872d69367
Paimon <n.h.shounen@gmail.com>
2021-12-30 19:44:05 +0700

    add response handler to select according to bot type

--- class/bot_handler_factory.py
+++ class/bot_handler_factory.py
@@ -1,4 +1,6 @@
+from ..helpers import consts
 from .bot_handler_abstract import BotHandlerAbstract
+from .bot_slack import BotSlack
 
 class BotHandlerFactory(BotHandlerAbstract):
     """
@@ -9,8 +11,13 @@ class BotHandlerFactory(BotHandlerAbstract):
     def __init__(self):
         self.handlers = {
             # TODO add type bot (ie slack, telegrams, discord etc)
+            consts.BOT_SLACK: BotSlack()
         }
     
-    def response_handler(self):
-        # TODO call response for each corresponding bot handler type
-        pass
+    def response_handler(self, bot_event, message):
+        bot_type = False  # TODO: add field bot type in command
+        handler = self.handlersl.get(bot_type)
+        if not handler:
+            message = 'Not found handler for bot type: {}'.format(bot_type)
+            raise Exception(message)
+        handler.response_handler(bot_event, message)
