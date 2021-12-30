6da39669417a3f121ade3e96632112d26ccac6e2
Paimon <n.h.shounen@gmail.com>
2021-12-30 19:43:46 +0700

    add class bot slack

--- class/bot_slack.py
+++ class/bot_slack.py
@@ -1,1 +1,9 @@
 from .bot_handler_abstract import BotHandlerAbstract
+
+class BotSlack(BotHandlerAbstract):
+    """
+    handle return response message to slack api
+    """
+
+    def response_handler(self):
+        pass
