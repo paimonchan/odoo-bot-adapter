707e0583cb3bc8066343d33c4d982b5bbbc40766
Paimon <n.h.shounen@gmail.com>
2022-01-02 22:30:44 +0700

    add fields bot type

--- models/bot_command.py
+++ models/bot_command.py
@@ -1,4 +1,7 @@
 from odoo import models, fields
+from ..helpers import consts
 
 class BotCommand(models.Model):
     _name = 'paimon.bot.command'
+
+    bot_type = fields.Selection(consts.BOT_TYPES)
--- models/bot_event.py
+++ models/bot_event.py
@@ -1,4 +1,7 @@
 from odoo import models, fields
+from ..helpers import consts
 
 class BotEvent(models.Model):
     _name = 'paimon.bot.event'
+
+    bot_type = fields.Selection(consts.BOT_TYPES)
