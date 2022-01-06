129eb4c92a5626a47e5ecf8f09b34a1924001bad
Paimon <n.h.shounen@gmail.com>
2022-01-06 20:27:51 +0700

    add state constant for model event

--- helpers/consts.py
+++ helpers/consts.py
@@ -5,3 +5,13 @@ BOT_TYPES = [
     (BOT_SLACK, 'slack'),
     (BOT_TELEGRAM, 'telegram'),
 ]
+
+EVENT_STATE_DRAFT = 'draft'
+EVENT_STATE_FAIL = 'fail'
+EVENT_STATE_DONE = 'done'
+
+EVENT_STATE_SELECTION = [
+    (EVENT_STATE_DRAFT, 'Draft'),
+    (EVENT_STATE_FAIL, 'Failure'),
+    (EVENT_STATE_DONE, 'Done'),
+]
